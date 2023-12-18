5bab17f27347c30a755b8cf17cc7281957670f39
mmtkocak <kocakmehmet4242@gmail.com>
2023-12-18 17:29:32 +0300

    Add project files.

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/CreateAboutCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Commands.AboutCommands;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class CreateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public CreateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAboutCommand command)
+        {
+            await _repository.CreateAsync(new About
+            {
+                Title = command.Title,
+                Description = command.Description,
+                ImageUrl = command.ImageUrl
+            });
+        }
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Results.AboutResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetAboutQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetAboutQueryResult
+            {
+                AboutID = x.AboutID,
+                Description = x.Description,
+                Title = x.Title,
+                ImageUrl = x.ImageUrl
+            }).ToList();
+        }
+    }
+}
