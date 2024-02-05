b32b0542bc8fee22f9b5eb098c3aa0251dfb27d0
Mehmet Koçak <mehmet.kocak@outlook.com.tr>
2024-02-05 16:40:54 +0300

    ...

---
+++ Frontends/UdemyCarBook.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public PartialViewResult AdminHeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminSideBarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
