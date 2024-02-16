57e92fd47e7df52cfa501672f3981a651244951e
Mehmet Koçak <mehmet.kocak@outlook.com.tr>
2024-02-16 15:10:32 +0300

    ...

---
+++ Frontends/UdemyCarBook.WebUI/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Blog")]
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
